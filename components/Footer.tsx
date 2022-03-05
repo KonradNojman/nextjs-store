6c633e56f1f54b18113c1ecc7d4cb2ded1ea7828
Konrad Nojman <konrad.nojman@htdevelopers.com>
2022-03-06 00:50:01 +0100

    Prepare structure of the main page

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="bg-gray-700 text-white text-2xl font-bold p-6">
+      Footer
+    </footer>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-700 text-white text-2xl font-bold p-6">
+      <nav>Nawigacja</nav>
+    </header>
+  );
+}
