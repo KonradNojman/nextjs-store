2d678f41f1e008e7ca67ced5f05e020fcdd3ac39
Konrad Nojman <konrad.nojman@htdevelopers.com>
2022-03-06 01:48:58 +0100

    Add active links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,38 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function Header() {
+  const router = useRouter();
+
+  const navigationLinks = [
+    {
+      slug: "/",
+      text: "Home",
+    },
+    {
+      slug: "/about",
+      text: "About",
+    },
+  ];
+
   return (
     <header className="bg-gray-700 text-white text-2xl font-bold p-6">
       <nav>
         <ul className="flex gap-6">
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
+          {navigationLinks.map((link, index) => (
+            <li
+              key={index}
+              className={
+                router.route === link.slug
+                  ? "selected text-blue-400"
+                  : "hover:underline"
+              }
+            >
+              <Link href={link.slug}>
+                <a>{link.text}</a>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
