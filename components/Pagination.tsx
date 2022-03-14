368c716022df89d8951dffdbbf5cba5f0f5eeb7a
Konrad Nojman <konrad.nojman@htdevelopers.com>
2022-03-15 00:58:59 +0100

    Add pagination component

---
+++ components/Pagination.tsx
@@ -0,0 +1,110 @@
+type PaginationProps = {
+  currentPage: number;
+  itemsAmount: number;
+  itemsPerPage: number;
+};
+
+// >1<,2...10
+// 1,>2<,3...10
+// 1,2,>3<,4...10
+// 1...3,>4<,5...10
+// ....
+// 1...6,>7<,8...10
+// 1...7,>8<,9,10
+// 1...8,>9<,10
+// 1...9,>10<
+
+// 1,2,>3<,4...10
+// 1...3,>4<,5...10
+// 1...4,>5<,6...10
+// 1...6,>7<,8...10
+// 1...7,>8<,9,10
+
+const PAGINATION_BORDER_ITEMS = 4;
+
+export const Pagination = ({
+  currentPage,
+  itemsAmount,
+  itemsPerPage,
+}: PaginationProps) => {
+  const amountOfPages = Math.ceil(itemsAmount / itemsPerPage);
+
+  const prevPagesArray =
+    currentPage < PAGINATION_BORDER_ITEMS
+      ? Array.from({ length: PAGINATION_BORDER_ITEMS }).map((_, i) => i + 1)
+      : [1];
+
+  const nextPagesArray =
+    currentPage > amountOfPages + 1 - PAGINATION_BORDER_ITEMS
+      ? Array.from({ length: PAGINATION_BORDER_ITEMS })
+          .map((_, i) => amountOfPages - i)
+          .reverse()
+      : [amountOfPages];
+
+  const currPagesArray = [currentPage - 1, currentPage, currentPage + 1];
+
+  const shouldShowMiddlePages =
+    currentPage >= PAGINATION_BORDER_ITEMS &&
+    currentPage <= amountOfPages + 1 - PAGINATION_BORDER_ITEMS;
+
+  const shouldShowAdditionalDots =
+    currentPage <= amountOfPages + 1 - PAGINATION_BORDER_ITEMS;
+
+  const defaultPaginationCssClasses =
+    "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium";
+  const activePaginationCssClasses =
+    "border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium";
+
+  return (
+    <nav className="border-t border-gray-200 px-4 flex items-center justify-between sm:px-0">
+      <div className="hidden md:-mt-px md:flex items-baseline">
+        {prevPagesArray.map((pageNumber) => (
+          <a
+            key={pageNumber}
+            href="#"
+            className={
+              currentPage === pageNumber
+                ? activePaginationCssClasses
+                : defaultPaginationCssClasses
+            }
+          >
+            {pageNumber}
+          </a>
+        ))}
+
+        <div className="text-2xl">...</div>
+        {shouldShowMiddlePages && (
+          <>
+            {currPagesArray.map((pageNumber) => (
+              <a
+                key={pageNumber}
+                href="#"
+                className={
+                  currentPage === pageNumber
+                    ? activePaginationCssClasses
+                    : defaultPaginationCssClasses
+                }
+              >
+                {pageNumber}
+              </a>
+            ))}
+            {shouldShowAdditionalDots && <div className="text-2xl">...</div>}
+          </>
+        )}
+        {nextPagesArray.map((pageNumber) => (
+          <a
+            key={pageNumber}
+            href="#"
+            className={
+              currentPage === pageNumber
+                ? activePaginationCssClasses
+                : defaultPaginationCssClasses
+            }
+          >
+            {pageNumber}
+          </a>
+        ))}
+      </div>
+    </nav>
+  );
+};
